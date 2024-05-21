2709a81559482e04ee227fe19582fc0f1c997668
Deena Lim <deena.lsy7@gmail.com>
2024-05-21 16:27:18 +0800

    deploy IDtoken

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
-/* 
+
 exports.handler = async (event, context) => {
     if (event.httpMethod === 'POST') {
       try {
@@ -97,28 +97,6 @@ exports.handler = async (event, context) => {
     }
     
   
-     */
+  
 
-    exports.handler = async (event, context) => {
-        if (event.httpMethod === 'POST') {
-          try {
-            // Parse the incoming JSON payload from the request body
-            const requestBody = JSON.parse(event.body);
-      
-            // Save the data to a database or perform other necessary operations
-            // ...
-      
-            // Return a success response
-            return {
-              statusCode: 200,
-              body: JSON.stringify({ message: 'POST request processed successfully' }),
-            };
-          } catch (error) {
-            // Return an error response if there was an issue processing the request
-            return {
-              statusCode: 400,
-              body: JSON.stringify({ error: 'Failed to process POST request' }),
-            };
-          }
-        }
-      };
+    
