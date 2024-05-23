d0ec697e9f845a39d620b45b58afad67299521f8
Deena Lim <deena.lsy7@gmail.com>
2024-05-23 18:21:35 +0800

    Remove .env

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,24 @@
 exports.handler = async (event) => {
       try {   
-        const dotenv = require("dotenv");
-        dotenv.config();        
+        const REACT_APP_SIGNATURE_PRIVATE_KEY = {
+          kty: "EC",
+          d: "0GlHbGc8vSnyiB-Lf4_im_WFwrxM0MJjkk96o1-K3JQ",
+          crv: "P-256",
+          x: "wg11s6ZpBc0my5gT-mYatTZRDhgStyd_0qARVBwAWa4",
+          y: "hlVoYWwlCuTMnm79Ppmf3RslIwDRhqdCCnm01PkhA2s"
+        };
+        const REACT_APP_ENCRYPTION_PRIVATE_KEY =  {
+          kty: "EC",
+          d: "p4YZHS0_BS4VMUayEtt38qi2sMdkhs4JRFlks7HJCD8",
+          crv: "P-256",
+          x: "0GR5oBa1FINjCZP_W-nR8Yqoz4E_9j7lgCuRPh9PZTA",
+          y: "0leGfxdQSJdtubopqhj5uhPVYV3LSd_yf3y2DdRD5No"
+        }
+    REACT_APP_CLIENT_ID="tLRDBkf1CNy5Rsi34mEKuOD5EpQAwjIq"
+    REACT_APP_JWTTOKENURL="https://stg-id.singpass.gov.sg"
+    REACT_APP_SPTOKENURL="https://stg-id.singpass.gov.sg/token"
+    REACT_APP_KID="testing123"
+    REACT_APP_REDIRECT_URI="https://singpassdemoapp.netlify.app/callback"     
         const code =event.queryStringParameters.code;
         console.log(code);
         const jose = require("jose");
@@ -9,31 +26,29 @@ exports.handler = async (event) => {
         const axios = require("axios");
         const alg = "ES256";
         //Signature Keys
-        console.log(process.env.REACT_APP_SIGNATURE_PRIVATE_KEY);
-        const jwk = JSON.parse(process.env.REACT_APP_SIGNATURE_PRIVATE_KEY)
-        const privateKey = await jose.importJWK(jwk, alg);
+        const privateKey = await jose.importJWK(REACT_APP_SIGNATURE_PRIVATE_KEY, alg);
         const nowTime = moment().unix();
         const futureTime = moment().add(2, "minutes").unix();
         const jwt = await new jose.SignJWT({
-          sub: process.env.REACT_APP_CLIENT_ID,
-          iss: process.env.REACT_APP_CLIENT_ID,
-          aud: process.env.REACT_APP_JWTTOKENURL,
+          sub: REACT_APP_CLIENT_ID,
+          iss: REACT_APP_CLIENT_ID,
+          aud: REACT_APP_JWTTOKENURL,
           iat: nowTime,
           exp: futureTime,
         })
           .setProtectedHeader({
             alg: "ES256",
-            kid: process.env.REACT_APP_KID,
+            kid: REACT_APP_KID,
             typ: "JWT",
           })
           .sign(privateKey);
        console.log(jwt);
-       const url = process.env.REACT_APP_SPTOKENURL;
+       const url = REACT_APP_SPTOKENURL;
         const { data } = await axios.post(
           url,
           new URLSearchParams({
-            client_id: process.env.REACT_APP_CLIENT_ID,
-            redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+            client_id: REACT_APP_CLIENT_ID,
+            redirect_uri: REACT_APP_REDIRECT_URI,
             code: code,
             client_assertion_type:
               "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
